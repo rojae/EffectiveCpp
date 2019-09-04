0df7339e67fdb817146a060ef5a025c830d28d12
rojae <41769568+rojae@users.noreply.github.com>
2019-09-04 13:39:33 +0900

    Add files via upload

---
+++ ch1/callbyvalue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+void addOne(int x);
+
+int main(){
+	int value;
+	std::cout << "Insert any number" << '\n';
+	std::cin >> value;
+	addOne(value);
+
+	return 0;
+}
+
+void addOne(int x){
+	x++;
+	std::cout << x << '\n';
+}
