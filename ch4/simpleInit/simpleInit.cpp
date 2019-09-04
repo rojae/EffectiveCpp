ff4bb8f1fe94676a97ea818de66681a951426ca5
rojae <41769568+rojae@users.noreply.github.com>
2019-09-04 19:03:19 +0900

    Add files via upload

---
+++ ch4/simpleInit/simpleInit.cpp
@@ -0,0 +1,19 @@
+/*
+ * Made person: rojae
+ * Made date: 2019.09.04
+ * --- code description ---
+ * 기본제공 타입의 객체는 직접 손으로 초기화를 하자
+ * 경우에 따라서 저절로 초기화가 되기도 하지만
+ * 그렇지 않은 경우도 있기 때문이다.
+ */
+
+#include <iostream>
+
+int main(){
+		int x = 0;		// x init
+		const char* str = "String String String";	// str init
+		double d;
+		std::cin >> d;		// d를 읽어들이면서 초기화 진헹
+		return 0;
+}
+
