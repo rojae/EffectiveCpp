568ff45c0828242d71f1cefcb760861400bf59b4
rojae <41769568+rojae@users.noreply.github.com>
2019-09-08 16:47:30 +0900

    Add files via upload

---
+++ ch7/virtual.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<unistd.h>
+
+class Base{
+		public:
+				Base(){
+						std::cout << "Base class Create" << '\n';
+				}
+				virtual ~Base(){
+						std::cout << "Base class delete" << '\n';
+				}
+};
+
+class Derived1 : public Base{
+		public:
+				Derived1(): Base(){
+						std::cout << "Derived1 class Create" << '\n';
+				}
+				~Derived1(){
+						std::cout << "Derived1 class delete" << '\n';
+				}
+};
+
+class Derived2 : public Base{
+		public:
+				Derived2(): Base(){
+						std::cout << "Derived2 class Create" << '\n';
+				}
+				~Derived2(){
+						std::cout << "Derived2 class delete" << '\n';
+				}
+};
+
+int main(){
+		Base* p1 = new Derived1;
+		Base* p2 = new Derived2;
+		delete p1;
+		delete p2;
+
+		return 0;
+}
