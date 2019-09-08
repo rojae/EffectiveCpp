64c91305dca746abb01cab47de0004ae5a85e34a
rojae <41769568+rojae@users.noreply.github.com>
2019-09-08 17:09:19 +0900

    Add files via upload

--- ch7/non_virtual.cpp
+++ ch7/non_virtual.cpp
@@ -1,41 +1,61 @@
+/*
+ * Made person: rojae
+ * Made date: 2019.09.08
+ * --- code description ---
+ *  1. 가상 소멸자를 사용했을 때와 아닐 때를 비교한다.
+ *  2. 가상 함수를 사용했을 때와 아닐 때를 비교한다.
+ *  우선 사용하지 않았을 경우.
+ */
 #include<iostream>
 #include<unistd.h>
 
 class Base{
-		public:
-				Base(){
-						std::cout << "Base class Create" << '\n';
-				}
-				~Base(){
-						std::cout << "Base class delete" << '\n';
-				}
+        public:
+                Base(){
+                        std::cout << "Base class Create" << '\n';
+                }
+                ~Base(){
+                        std::cout << "Base class delete" << '\n';
+                }
+                void what(){
+                        std::cout << "Base's what" << '\n';
+                }
 };
 
 class Derived1 : public Base{
-		public:
-				Derived1(): Base(){
-						std::cout << "Derived1 class Create" << '\n';
-				}
-				~Derived1(){
-						std::cout << "Derived1 class delete" << '\n';
-				}
+        public:
+                Derived1(): Base(){
+                        std::cout << "Derived1 class Create" << '\n';
+                }
+                ~Derived1(){
+                        std::cout << "Derived1 class delete" << '\n';
+                }
+                void what(){
+                        std::cout << "Derived1 what" << '\n';
+                }
 };
 
 class Derived2 : public Base{
-		public:
-				Derived2(): Base(){
-						std::cout << "Derived2 class Create" << '\n';
-				}
-				~Derived2(){
-						std::cout << "Derived2 class delete" << '\n';
-				}
+        public:
+                Derived2(): Base(){
+                        std::cout << "Derived2 class Create" << '\n';
+                }
+                ~Derived2(){
+                        std::cout << "Derived2 class delete" << '\n';
+                }
+                void what(){
+                        std::cout << "Derived2 what" << '\n';
+                }
 };
 
 int main(){
-		Base* p1 = new Derived1;
-		Base* p2 = new Derived2;
-		delete p1;
-		delete p2;
+        Base* p1 = new Derived1;
+        Base* p2 = new Derived2;
+        p1->what();
+        p2->what();
+        delete p1;
+        delete p2;
 
-		return 0;
+        return 0;
 }
+
--- ch7/virtual.cpp
+++ ch7/virtual.cpp
@@ -1,3 +1,10 @@
+/*
+ * Made person: rojae
+ * Made date: 2019.09.08
+ * --- code description ---
+ *  가상 소멸자와 가상 함수를 사용했을 때를 알아본다.
+ *  사용했을 때 파생 클래스 소멸자 및 함수가 정상적으로 동작한다는 것을 알 수 있다.
+ */
 #include<iostream>
 #include<unistd.h>
 
@@ -9,6 +16,9 @@ class Base{
 				virtual ~Base(){
 						std::cout << "Base class delete" << '\n';
 				}
+				virtual void what(){
+						std::cout << "Base's what" << '\n';
+				}
 };
 
 class Derived1 : public Base{
@@ -18,6 +28,9 @@ class Derived1 : public Base{
 				}
 				~Derived1(){
 						std::cout << "Derived1 class delete" << '\n';
+				}
+				void what(){
+						std::cout << "Derived1 what" << '\n';
 				}
 };
 
@@ -29,11 +42,16 @@ class Derived2 : public Base{
 				~Derived2(){
 						std::cout << "Derived2 class delete" << '\n';
 				}
+				void what(){
+						std::cout << "Derived2 what" << '\n';
+				}
 };
 
 int main(){
 		Base* p1 = new Derived1;
 		Base* p2 = new Derived2;
+		p1->what();
+		p2->what();
 		delete p1;
 		delete p2;
 
